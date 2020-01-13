656bad2a8b0baebb564d1eea1e103cbcb270ceef
cerebral-caller-0985 <ashaffe@gmail.com>
2020-01-13 02:41:38 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,18 +7,22 @@ def join_ingredients(src)
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
 
-  src.map do |pair|
+  return src.map do |pair|
     "I love #{pair[0]} and #{pair[1]} on my pizza"
   end
-  src
+
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-  src.map do |pair|
-    Math.max(pair[0],pair[1])
+  return src.map do |pair|
+    if(pair[0] > pair[1])
+      pair[0]
+    else
+      pair[1]
+    end
   end
   src
 end
